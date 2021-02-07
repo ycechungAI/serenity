ycechungAI/serenity

commit 57e10eadac01273cc4c0bcb681aa9381cacef0b3

LibGfx: Don't reject valid GIF animations with interlaced frames
We were returning early from the deinterlacing loop after the very last
pass, but we should just let the outer loop finish and return instead.

This makes the Netscape animation on https://timmorgan.dev work. :^)

Userland/Libraries/LibGfx/GIFLoader.cpp
@@ -401,10 +401,8 @@ static bool decode_frame(GIFLoadingContext& context, size_t frame_index)
                     if (image.interlaced) {
                         if (row + INTERLACE_ROW_STRIDES[interlace_pass] >= image.height) {
                             ++interlace_pass;
-                            // FIXME: We could probably figure this out earlier and fail before doing a bunch of work.
-                            if (interlace_pass >= 4)
-                                return false;
-                            row = INTERLACE_ROW_OFFSETS[interlace_pass];
+                            if (interlace_pass < 4)
+                                row = INTERLACE_ROW_OFFSETS[interlace_pass];
                         } else {
                             row += INTERLACE_ROW_STRIDES[interlace_pass];
                         }

commit 0a65dac938b839f7d9aa08f80399bab00cc0c59a

LibGfx: Fix global-buffer-overflow in interlaced GIF decode
Regressed with 57e10eadac01273cc4c0bcb681aa9381cacef0b3 and immediately
caught by oss-fuzz: https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=30507

Userland/Libraries/LibGfx/GIFLoader.cpp
@@ -404,7 +404,8 @@ static bool decode_frame(GIFLoadingContext& context, size_t frame_index)
                             if (interlace_pass < 4)
                                 row = INTERLACE_ROW_OFFSETS[interlace_pass];
                         } else {
-                            row += INTERLACE_ROW_STRIDES[interlace_pass];
+                            if (interlace_pass < 4)
+                                row += INTERLACE_ROW_STRIDES[interlace_pass];
                         }
                     } else {
                         ++row;