ycechungAI/serenity

commit 0fcb9efd86da4c15a1aee87503348c5bee875c51

Kernel: Return an error when unmap finds no intersecting region
We currently always crash if a user attempts to unmap a range that
does not intersect with an existing region, no matter the size. This
happens because we will never explicitly check to see if the search
for intersecting regions found anything, instead loop over the results,
which might be an empty vector. We then attempt to deallocate the
requested range from the `RangeAllocator` unconditionally, which will
be invalid if the specified range is not managed by the RangeAllocator.
We will assert validating m_total_range.contains(..) the range we are
requesting to deallocate.

This fix to this is straight forward, error out if we weren't able to
find any intersections.

You can get stress-ng to attempt this pattern with the following
arguments, which will attempt to unmap 0x0 through some large offset:

```
stress-ng --vm-segv 1
```

Fixes: #8483

Co-authored-by: Federico Guerinoni <guerinoni.federico@gmail.com>

Kernel/VM/Space.cpp
@@ -45,7 +45,6 @@ KResult Space::unmap_mmap_range(VirtualAddress addr, size_t size)
     auto range_or_error = Range::expand_to_page_boundaries(addr.get(), size);
     if (range_or_error.is_error())
         return range_or_error.error();
-
     auto range_to_unmap = range_or_error.value();
 
     if (!is_user_range(range_to_unmap))
@@ -92,6 +91,8 @@ KResult Space::unmap_mmap_range(VirtualAddress addr, size_t size)
 
     // Try again while checking multiple regions at a time.
     auto const& regions = find_regions_intersecting(range_to_unmap);
+    if (regions.is_empty())
+        return EINVAL;
 
     // Check if any of the regions is not mmap'ed, to not accidentally
     // error out with just half a region map left.

commit 649fa44fa74e7b74c074d37a484fc2bdccf40ca3

Kernel: Unmapping a non-mapped region with munmap() should be a no-op
Not a regression per se from 0fcb9efd86da4c15a1aee87503348c5bee875c51
since we were crashing before that which is obviously worse.

Kernel/VM/Space.cpp
@@ -92,7 +92,7 @@ KResult Space::unmap_mmap_range(VirtualAddress addr, size_t size)
     // Try again while checking multiple regions at a time.
     auto const& regions = find_regions_intersecting(range_to_unmap);
     if (regions.is_empty())
-        return EINVAL;
+        return KSuccess;
 
     // Check if any of the regions is not mmap'ed, to not accidentally
     // error out with just half a region map left.

Tests/Kernel/TestMunMap.cpp
@@ -10,8 +10,7 @@
 
 TEST_CASE(munmap_zero_page)
 {
-    // munmap of the unmapped zero page should always fail.
+    // munmap of the unmapped zero page should always "succeed".
     auto res = munmap(0x0, 0xF);
-    EXPECT_EQ(res, -1);
-    EXPECT_EQ(errno, EINVAL);
+    EXPECT_EQ(res, 0);
 }